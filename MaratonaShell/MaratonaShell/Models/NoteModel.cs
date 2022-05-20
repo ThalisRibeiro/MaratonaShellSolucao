b1495ce1fdb2117b9e68f67c40859668530c082b
Thalis Ribeiro <thalis199@outlook.com>
2022-05-20 20:58:39 -0300

    Criacao do modelo das notas

---
+++ MaratonaShell/MaratonaShell/Models/NoteModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace MaratonaShell.Models
+{
+    public class NoteModel
+    {
+        string texto;
+        DateTime hora;
+        public string Texto
+        {
+            get { return texto; }
+        }
+        public string Hora
+        {
+            get { return hora.ToString(); }
+        }
+
+        public NoteModel(string textoRecebido)
+        {
+            hora = DateTime.Now;
+            texto = textoRecebido;
+        }
+    }
+}
