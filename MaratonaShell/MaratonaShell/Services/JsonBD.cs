ba6e8a376862ba613da7f9fc92d25c162be1ebe3
Thalis Ribeiro <thalis199@outlook.com>
2022-05-25 16:59:00 -0300

    base da classe

--- MaratonaShell/MaratonaShell/Services/JsonBD.cs
+++ MaratonaShell/MaratonaShell/Services/JsonBD.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace MaratonaShell.Services
 {
     public class JsonBD
     {
+        readonly string path;
+        public void Serializador()
+        {
+
+        }
+
+        public void Deserializador()
+        {
+
+        }
     }
 }
