219465fec4595ead499e26c9d20e0982d44562b8
Thalis Ribeiro <thalis199@outlook.com>
2022-05-25 17:06:24 -0300

    criacao do serializador

--- MaratonaShell/MaratonaShell/Services/JsonBD.cs
+++ MaratonaShell/MaratonaShell/Services/JsonBD.cs
@@ -1,18 +1,27 @@
-﻿using System;
+﻿using MvvmHelpers;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using Newtonsoft.Json;
+using MaratonaShell.Models;
 
 namespace MaratonaShell.Services
 {
     public class JsonBD
     {
-        readonly string path;
-        public void Serializador()
+        readonly string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Arquivo.json");
+        public void Serializador(ObservableRangeCollection<NoteModel> colecao)
         {
+            string json = JsonConvert.SerializeObject(colecao);
+            SalvaJson(json);
 
         }
+        void SalvaJson(string json)
+        {
+            File.WriteAllText(path, json);
+        }
+
 
         public void Deserializador()
         {
